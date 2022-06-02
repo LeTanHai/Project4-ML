a5a7c5e4f7205526522637afec31142bbf6cfbb6
Tan Hai <tanhaile98@gmail.com>
2022-06-02 22:31:57 +0700

    update code fix comment review

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -15,7 +15,7 @@ dockerpath=letanhai1998/devops-repo-project4:latest
 kubectl get pods
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward pod/devop-project4-app-54f5dd8f5c-kk89x --address 0.0.0.0 8080:80
+kubectl port-forward pod/devop-project4-app-54f5dd8f5c-htjlf --address 0.0.0.0 8080:80
 
 # Logs
-kubectl logs devop-project4-app-54f5dd8f5c-kk89x
+kubectl logs devop-project4-app-54f5dd8f5c-htjlf
